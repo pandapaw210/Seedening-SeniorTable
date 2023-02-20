7a68ade92006478bd680d18e13d740ab0df88d55
Serim00 <125899792+Serim00@users.noreply.github.com>
2023-02-21 01:02:09 +0900

    Delete urls.py
    Signed-off-by: Serim00 <125899792+Serim00@users.noreply.github.com>

--- preventGame/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-	path('', views.preventGame, name='preventGame'),
-    path('sudoku/', views.sudoku, name='sudoku'),
-    path('flappybird/', views.flappybird, name='flappybird'),
-]
