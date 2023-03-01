c9e8f0e50c5cf27a89b9d33b2f3be6c286e5706b
pandapaw210 <120902345+pandapaw210@users.noreply.github.com>
2023-03-01 15:59:10 +0900

    Update urls.py
    Signed-off-by: pandapaw210 <120902345+pandapaw210@users.noreply.github.com>

--- helpingData/urls.py
+++ helpingData/urls.py
@@ -3,7 +3,8 @@ from . import views
 
 urlpatterns = [
 	path('', views.helpingData, name='helpingData'),
-    path('books/', views.books, name='books'),
+   	path('books/', views.books, name='books'),
+	path('booksEdit/', views.booksEdit, name='booksEdit'),
 	path('helpingDataTip1/', views.helpingDataTip1, name='helpingDataTip1'),
 	path('helpingDataTip2/', views.helpingDataTip2, name='helpingDataTip2'),
 	path('helpingDataTip3/', views.helpingDataTip3, name='helpingDataTip3'),
