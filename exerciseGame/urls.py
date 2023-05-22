fc37665cc7b5fc8b89b2a3ef2023573ae5d06c3e
Serim00 <125899792+Serim00@users.noreply.github.com>
2023-05-22 21:12:15 +0900

    Add files via upload
    Signed-off-by: Serim00 <125899792+Serim00@users.noreply.github.com>

--- exerciseGame/urls.py
+++ exerciseGame/urls.py
@@ -6,10 +6,5 @@ urlpatterns = [
 	path('cube3D/', views.cube3D, name='cube3D'),
 	path('piano/', views.piano, name='piano'),
 	path('block/', views.block, name='block'),
-	path('ball_breaker/', views.ball_breaker, name='ball_breaker'),
-	path('pong/', views.pong, name='pong'),
-	path('shooting/', views.shooting, name='shooting'),
-	path('jump/', views.jump, name='jump'),
-	path('memorize/', views.memorize, name='memorize'),
-	path('car/', views.car, name='car'),
+	path('ball_breaker/', views.ball_breaker, name='ball_breaker')
 ]
