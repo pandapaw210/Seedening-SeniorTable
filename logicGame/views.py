098c79549b3c18ed48d0d0afb4e3d862fad0b53e
pandapaw210 <120902345+pandapaw210@users.noreply.github.com>
2023-05-23 01:09:46 +0900

    Add files via upload
    Signed-off-by: pandapaw210 <120902345+pandapaw210@users.noreply.github.com>

---
+++ logicGame/views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+
+def logicGame(request):
+	return render(request, 'logicGame.html', {})
+
+def memoryGame(request):
+	return render(request, 'memoryGame.html', {})
+
+def puzzle(request):
+	return render(request, 'puzzle.html', {})
+
+def game2048(request):
+	return render(request, '2048.html', {})
+
+def hanoitower(request):
+	return render(request, 'hanoitower.html', {})
+
+def Omok(request):
+	return render(request, 'Omok.html', {})
+
+def binairo(request):
+	return render(request, 'binairo.html', {})
+
+def sudoku(request):
+	return render(request, 'sudoku.html', {})
+
+def square(request):
+	return render(request, 'square.html', {})
+
+def dots(request):
+	return render(request, 'dots.html', {})
+
+def freecell(request):
+	return render(request, 'freecell.html', {})
+
+def hanoi(request):
+	return render(request, 'hanoi.html', {})
+
