0d8f532491fcec9aede1b14ef45f1be5bb79b715
pandapaw210 <120902345+pandapaw210@users.noreply.github.com>
2023-05-22 22:04:57 +0900

    Update urls.py
    Signed-off-by: pandapaw210 <120902345+pandapaw210@users.noreply.github.com>

--- logicGame/urls.py
+++ logicGame/urls.py
@@ -11,6 +11,4 @@ urlpatterns = [
 	path('binairo/', views.binairo, name='binairo'),
     	path('sudoku/', views.sudoku, name='sudoku'),
    	path('square/', views.square, name='square'),
-	
-	path('hanoi/', views.hanoi, name='hanoi'),
 ]
