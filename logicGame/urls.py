11682f67aa1f2a88429a1b20fdc8af3436404474
pandapaw210 <120902345+pandapaw210@users.noreply.github.com>
2023-07-31 16:47:12 +0900

    Update urls.py
    Signed-off-by: pandapaw210 <120902345+pandapaw210@users.noreply.github.com>

--- logicGame/urls.py
+++ logicGame/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
 from . import views
-from . import url
 
 
 urlpatterns = [
@@ -13,6 +12,10 @@ urlpatterns = [
 	path('binairo/', views.binairo, name='binairo'),
    	path('sudoku/', views.sudoku, name='sudoku'),
     	path('square/', views.square, name='square'),
+
+	path('dots/', views.dots, name='dots'),
+    	path('freecell/', views.freecell, name='freecell'),
+   	path('hanoi/', views.hanoi, name='hanoi'),
    
 ]
 
