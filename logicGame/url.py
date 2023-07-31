77057ed53618c528f7c7e556dcf59e36e16d91b1
pandapaw210 <120902345+pandapaw210@users.noreply.github.com>
2023-07-31 16:47:55 +0900

    Delete url.py
    Signed-off-by: pandapaw210 <120902345+pandapaw210@users.noreply.github.com>

--- logicGame/url.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-
-urlpatterns = [
-    path('dots/', views.dots, name='dots'),
-    path('freecell/', views.freecell, name='freecell'),
-    path('hanoi/', views.hanoi, name='hanoi'),
-]
