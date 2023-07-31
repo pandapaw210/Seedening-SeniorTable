fbf3e3b3443fb0a078aab1f23cb602041f7eb77c
pandapaw210 <120902345+pandapaw210@users.noreply.github.com>
2023-07-31 16:58:47 +0900

    Update urls.py

--- logicGame/urls.py
+++ logicGame/urls.py
@@ -3,15 +3,5 @@ from . import views
 
 urlpatterns = [
 	path('', views.logicGame, name='logicGame'),
-	path('memoryGame/', views.memoryGame, name='memoryGame'),
-	path('puzzle/', views.puzzle, name='puzzle'),
-	path('2048/', views.game2048, name='2048'),
-	path('hanoitower/', views.hanoitower, name='hanoitower'),
-	path('Omok/', views.Omok, name='Omok'),
-	path('binairo/', views.binairo, name='binairo'),
-    path('sudoku/', views.sudoku, name='sudoku'),
-    path('square/', views.square, name='square'),
-    path('dots/', views.dots, name='dots'),
-    path('freecell/', views.freecell, name='freecell'),
-    path('hanoi/', views.hanoi, name='hanoi'),
+	
 ]
